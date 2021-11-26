ffafd4f02d59c74fae834d3756668e98211123b7
AngelVigliengo <angel.vigliengo@limayrac.fr>
2021-11-26 11:36:26 +0100

    Initial commit

---
+++ src/main/java/fr/limayrac/banque/controller/ClientController.java
@@ -0,0 +1,52 @@
+package fr.limayrac.banque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/client", method = RequestMethod.GET)
+public class ClientController {
+
+    @RequestMapping(value = "/lister")
+    public ModelAndView newHome(HttpServletRequest req, HttpServletResponse rep){
+
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        model.put("Angel", "Vigliengo");
+        return new ModelAndView("listeClient","data", model);
+    }
+
+    @RequestMapping(value = "/lister/{client}")
+    @ResponseBody
+    public String detailCompte
+            (@PathVariable String client) {
+        return "url du client :"+ client;
+    }
+
+    @RequestMapping(path = "/créer")
+    @ResponseBody
+    public String creationClient(){
+        return "création d'un client";
+    }
+
+    @RequestMapping(path = "/editer")
+    @ResponseBody
+    public String editionClient(){
+        return "édition d'un client";
+    }
+
+    @RequestMapping(path = "/effacer")
+    @ResponseBody
+    public String effacerClient(){
+        return "effacer un client";
+    }
+}
---
+++ src/main/java/fr/limayrac/banque/model/Compte.java
@@ -0,0 +1,18 @@
+package fr.limayrac.banque.model;
+
+public class Compte {
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String profession;
+    private String email;
+    private String motDepasse;
+    private String sexe;
+    private boolean celibataire;
+    private String remarques;
+
+    public Compte(){
+        super();
+    }
+    
+}
